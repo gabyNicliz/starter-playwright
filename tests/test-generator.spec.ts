b082d1c18f577b349e8c184b6eaf540c63044b9a
gabyNicliz <gabynicliz93@gmail.com>
2023-05-03 00:03:27 -0300

    First commit

---
+++ tests/test-generator.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://demo.playwright.dev/todomvc/');
+  await page.goto('https://demo.playwright.dev/todomvc/#/');
+  await page.getByPlaceholder('What needs to be done?').click();
+  await page.getByPlaceholder('What needs to be done?').fill('hello');
+  await page.getByPlaceholder('What needs to be done?').press('Enter');
+});
